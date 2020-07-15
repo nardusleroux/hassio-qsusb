a96d0d586c9f62d486b87d7435ab44000a64de29
Johann Kellerman <kellerza@gmail.com>
2020-07-15 23:48:21 +0200

    Fix amd64 arch (add environment debug info), update docs (no CRLF), icons from brands

--- qsusb/run.sh
+++ qsusb/run.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 set -e
 
+echo . && echo ENVIRONMENT - DOCKER BUILD
+cat /qsusb/env.txt
+echo . && echo ENVIRONMENT - DOCKER RUN
+printenv
+
 CONFIG_PATH=/data/options.json
 DEVICES=$(jq --raw-output '.devices | length' $CONFIG_PATH)
-PORT=$(jq --raw-output ".port" $CONFIG_PATH)
 
 # Create devices file with input from options.json
 for (( i=0; i < "$DEVICES"; i++ )); do
     STRING=$(jq --raw-output ".devices[$i].string" $CONFIG_PATH)
-
     {
       echo "$STRING"
     } >> /qsusb/QSUSB/devices
 done
 
-if [ ! -f "$PORT" ]; then
-  sed -i "s|2020|$PORT|g" /qsusb/QSUSB/setup.ini
-fi
+echo . && echo DEVICES CONFIG
+cat /qsusb/QSUSB/devices
+
+echo . && echo LS
+ls /qsusb/QSUSB/qsusb* -l
 
 echo "Starting"
-/qsusb/QSUSB/qsusb
+[ -f /qsusb/QSUSB/qsusb64 ] && /qsusb/QSUSB/qsusb64
+[ -f /qsusb/QSUSB/qsusb ] && /qsusb/QSUSB/qsusb
